d481fc2ff937f3df196b3aa72f88670ab2b0644e
shubhamiiit1234 <shubhamkumar88769@gmail.com>
2026-06-03 00:26:31 +0530

    chore: db connection & migration file

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"login/internal/database"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	fmt.Println("Login System!")
+
+	connection := "host=localhost port=5432 user=postgres password=mysecretpassword dbname=Login sslmode=disable"
+	err := database.InitializeDB(connection)
+	if err != nil {
+		fmt.Println("error initializing database: ", err.Error())
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module login
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.3.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.0 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
